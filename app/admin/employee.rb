7fbc22accca9b201ddac226553082d52d30a8082
Juliat Joy <juliatjoy@gmail.com>
2016-02-20 13:02:17 +0530

    Added fields in ActiveAdmin

--- app/admin/employee.rb
+++ app/admin/employee.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Employee do
   filter :name
 
   permit_params :name, :date_of_joining, :date_of_birth, :bank_name, :bank_account_number,
-                :bank_ifsc_code, :bank_branch_name, :pancard, :email_address
+                :bank_ifsc_code, :bank_branch_name, :pancard, :email_address, :employee_id, :designation, :fathers_name, :pf_account, :esi_account
 
   index do
     column :name
@@ -19,8 +19,10 @@ ActiveAdmin.register Employee do
   end
 
   show do
-    attributes_table :name, :email_address, :date_of_birth, :date_of_joining, :pancard, :bank_name,
-                     :bank_ifsc_code, :bank_branch_name, :bank_account_number,
+    attributes_table :name,:employee_id, :email_address, :fathers_name, :date_of_birth,
+                     :date_of_joining, :pancard, :designation,
+                     :bank_name, :bank_ifsc_code, :bank_branch_name, :bank_account_number,
+                     :pf_account, :esi_account
                      :address
     active_admin_comments
   end
@@ -28,14 +30,19 @@ ActiveAdmin.register Employee do
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "employee" do
       f.input :name
+      f.input :employee_id
+      f.input :fathers_name
       f.input :email_address
       f.input :date_of_birth, :as => :datepicker
       f.input :date_of_joining, :as => :datepicker
       f.input :pancard
+      f.input :designation
       f.input :bank_name
       f.input :bank_account_number
       f.input :bank_ifsc_code
       f.input :bank_branch_name
+      f.input :pf_account
+      f.input :esi_account
       f.input :address
       f.input :id, as: :hidden, :value => Employee.find_by_id(params[:id])
     end
