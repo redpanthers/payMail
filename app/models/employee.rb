1dddb69554054ae6b8f90d531bca31545c0d3024
Manu S Ajith <neo@codingarena.in>
2016-02-26 18:29:44 +0530

    Delegate department name in Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,9 @@
 class Employee < ActiveRecord::Base
-  attr_encrypted :pancard, :key => Rails.application.secrets.encryption_key, :attribute => 'encrypted_pancard'
-  attr_encrypted :bank_name, :key => Rails.application.secrets.encryption_key, :attribute => 'encrypted_bank_name'
-  attr_encrypted :bank_account_number, :key => Rails.application.secrets.encryption_key, :attribute => 'encrypted_bank_account_number'
-  attr_encrypted :bank_branch_name, :key => Rails.application.secrets.encryption_key, :attribute => 'encrypted_bank_branch_name'
-  attr_encrypted :bank_ifsc_code, :key => Rails.application.secrets.encryption_key, :attribute => 'encrypted_bank_ifsc_code'
+  attr_encrypted :pancard, key: Rails.application.secrets.encryption_key, attribute: 'encrypted_pancard'
+  attr_encrypted :bank_name, key: Rails.application.secrets.encryption_key, attribute: 'encrypted_bank_name'
+  attr_encrypted :bank_account_number, key: Rails.application.secrets.encryption_key, attribute: 'encrypted_bank_account_number'
+  attr_encrypted :bank_branch_name, key: Rails.application.secrets.encryption_key, attribute: 'encrypted_bank_branch_name'
+  attr_encrypted :bank_ifsc_code, key: Rails.application.secrets.encryption_key, attribute: 'encrypted_bank_ifsc_code'
 
   validates :name, :pancard, :bank_ifsc_code, :bank_name, :bank_branch_name, :bank_account_number, :employee_id, :designation, :fathers_name, presence: true
 
@@ -11,4 +11,6 @@ class Employee < ActiveRecord::Base
 
   has_many :pay_slips
   belongs_to :department
+
+  delegate :name, to: :department, prefix: true, allow_nil: true
 end
